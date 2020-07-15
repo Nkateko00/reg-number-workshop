da17da34fa9c77c284f975d0eb59e1880764b317
avermeulen <andre.vermeulen@gmail.com>
2020-07-15 14:23:46 +0200

    added cleaned up workshop code

--- books.js
+++ books.js
@@ -3,3 +3,35 @@ const bookTemplateElem = document.querySelector(".bookTemplate");
 const message1 = document.querySelector(".message1");
 const message2 = document.querySelector(".message2");
 
+console.log(bookTemplateElem.innerHTML);
+
+const booksTemplate = Handlebars.compile(bookTemplateElem.innerHTML);
+
+const users = [
+	{
+		firstName: "Kagiso",
+		bookCount: 7,
+		dayCount: 5
+	},
+	{
+		firstName: "Meycayle",
+		bookCount: 17,
+		dayCount: 2
+	},
+	{
+		firstName: "Andy",
+		bookCount: 6,
+		dayCount: 5
+	},
+]
+
+
+message1.innerHTML = booksTemplate({
+	users
+});
+
+// message2.innerHTML = booksTemplate({
+// 	firstName: "Meycayle",
+// 	bookCount: 17,
+// 	dayCount: 2
+// });
