77cadbb87e884b22bc1ed46836276e3e0bad8c1f
Nkateko00 <tekonku@icloud.com>
2020-07-30 23:31:34 +0200

    templating

--- books.js
+++ books.js
@@ -3,3 +3,26 @@ const bookTemplateElem = document.querySelector(".bookTemplate");
 const message1 = document.querySelector(".message1");
 const message2 = document.querySelector(".message2");
 
+const booksTemplates = Handlebars.compile(bookTemplateElem.innerHTML);
+
+const users = [{
+    firstName : "Teko" ,
+    bookCount : 8,
+    dayCount : 5,
+},
+{
+    firstName : "Zizou",
+    bookCount : 3,
+    dayCount : 5,
+
+},
+{
+    firstName : "Ashley",
+    bookCount :9,
+    dayCount : 6,
+},
+]
+
+    message1.innerHTML = booksTemplates ({
+        users
+    });
